3683b5a0ad4b9bc835e958565365d2648ac53214
sbddhrju <sahitya.buddharaju@gmail.com>
2024-02-14 17:30:55 -0600

    Github deployment configuration

--- mentor-matrix/vite.config.ts
+++ mentor-matrix/vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from 'tailwindcss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/mentor-matrix",
   css: {
     postcss: {
       plugins: [tailwindcss()],
