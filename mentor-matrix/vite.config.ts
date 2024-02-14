8911d576219c9c33b60a31d9443a41d2bba73429
sbddhrju <sahitya.buddharaju@gmail.com>
2024-02-14 17:39:55 -0600

    Vite config change for deployment

--- mentor-matrix/vite.config.ts
+++ mentor-matrix/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from 'tailwindcss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mentor-matrix",
+  base: '/mentor-matrix/',
   css: {
     postcss: {
       plugins: [tailwindcss()],
