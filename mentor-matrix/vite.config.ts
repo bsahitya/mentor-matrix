531f8c8588639c976bf10013126f18e534e62558
sbddhrju <sahitya.buddharaju@gmail.com>
2024-02-16 10:20:44 -0600

    Changed cache dependency path in deploy.yml

--- mentor-matrix/vite.config.ts
+++ mentor-matrix/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from 'tailwindcss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/mentor-matrix/',
   css: {
     postcss: {
       plugins: [tailwindcss()],
